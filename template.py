6392ed95b1d673fb0a595963da4a2732b59bfeaa
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-06-03 14:37:51 +0700

    feat: data ingestion notebook added

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 project_name = "textSummarizer"
 
@@ -12,11 +12,12 @@ list_of_files = [
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
-    f"project/{project_name}/logging/__init__.py",
-    f"project/{project_name}/config/__init__.py",
-    f"project/{project_name}/config/configuration.py",
-    f"project/{project_name}/entity/__init__.py",
-    f"project/{project_name}/constants/__init__.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
@@ -39,6 +40,5 @@ for filepath in list_of_files:
         with open(filepath, 'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
-            
     else:
-        logging.info(f"{filename} is already exists")
+        logging.info(f"{filename} already exists")
